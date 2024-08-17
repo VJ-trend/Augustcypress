8e0b01b2e4819b502701a7fd538d6b1e7f6821b5
vijay <vijaysparks21@gmail.com>
2024-08-17 16:51:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,62 @@
+//printing statement
+//console.log()
+
+// console.log("hello im java_script") 
+
+
+//datatypes : 
+
+//primitive & non primitive
+
+//primitive datatypes : 
+
+//string   =>   "dfjdhfjsjdfh"
+//number  = > number and also decimal
+//boolean   => true or false
+//undefined
+//null
+
+//non primitive datatypes : 
+
+//array : [ ] :Collection of data and values in a square brackets is called array
+
+var arr = ["hello","hi","howareyou"]
+console.log(arr);
+
+//object : 
+  //collection of key and value pairs
+const obj = {
+
+    //key     //value
+     name : "vijay",
+     age : 20,
+     dep : "qa tester"
+}
+
+console.log(obj);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
